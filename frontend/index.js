7a61fa435f88ff41947a11289082598cde6de2e3
Yannan Zhang <zhangyannan199744@outlook.com>
2024-03-12 17:04:07 +0200

    3/12 add modify and delete functions

--- frontend/index.js
+++ frontend/index.js
@@ -38,8 +38,8 @@ const fetchAddUserData = async (info) => {
 
 // input type for 'add'
 const addInfo = {
-    id : 0,
-    data : [
+    id: 0,
+    data: [
         {
             "dataContent": {
                 "content": "add new content for user 0",
@@ -67,9 +67,9 @@ const fetchRemoveUserData = async (info) => {
 
 // input type for 'remove'
 const removeInfo = {
-    id : 0,
-    data : {
-        dataId : 0,
+    id: 0,
+    data: {
+        dataId: 1,
     },
 }
 
@@ -91,10 +91,10 @@ const fetchChangeUserInfo = async (info) => {
 
 // input type for 'change'
 const changeInfo = {
-    id : 0,
-    data : {
-        dataId : 0,
-        dataContent : {
+    id: 0,
+    data: {
+        dataId: 0,
+        dataContent: {
             "content": "after change",
             "importance": "normal"
         }
@@ -107,25 +107,96 @@ const changeInfo = {
 // console.log(fetchChangeUserInfo(changeInfo))
 
 // load userinfo to webpage
-const getDataList = async(info) => {
+const getDataList = async (info) => {
     try {
         const container = document.getElementById('container')
         const response = await fetchGetUserInfo(info)
-        response.data.forEach(element => {
+        response.data.forEach(async element => {
+            let index = response.data.indexOf(element)
             const dataBlock = document.createElement('div')
             const updateTime = moment(element.dataContent.updateTime)
-            const content = element.dataContent.content
-            const importance = element.dataContent.importance
+            let content = element.dataContent.content
+            let importance = element.dataContent.importance
             dataBlock.innerHTML = `
+            <form action='' method='post'>
                 <span> ${updateTime} </span>
-                <span> ${content} </span>
-                <span> ${importance} </span>
+                <span id='content'> ${content} </span>
+                <span id='importance'> ${importance} </span>
+            </form>    
             `
+            // add buttons
+            const methodsArea = document.createElement('div')
+            // remove
+            const labelRemove = document.createElement('label')
+            labelRemove.innerHTML = `
+                <span>delete item</span>
+            `
+            labelRemove.for = `remove_${index}`
+            const removeBtn = document.createElement('input')
+            removeBtn.id = `remove_${index}`
+            removeBtn.name = 'remove'
+            removeBtn.type = 'button'
+            removeBtn.value = 'remove'
+            removeBtn.addEventListener('click', async () => {
+                const response = await fetchRemoveUserData({
+                    id: 0,
+                    data: {
+                        dataId: index,
+                    }
+                })
+                if (response.valid === true) {
+                    dataBlock.innerHTML = ''
+                }
+            })
+            // change
+            const labelModify = document.createElement('label')
+            labelModify.innerHTML = `
+            <span>modify item</span>
+            `
+            labelModify.for = `modify_${index}`
+            const modifyBtn = document.createElement('input')
+            modifyBtn.id = `modify_${index}`
+            modifyBtn.name = 'modify'
+            modifyBtn.type = 'button'
+            modifyBtn.value = 'modify'
+            modifyBtn.addEventListener('click', async (e) => {
+                e.preventDefault()
+                const contentElement = document.getElementById('content')
+                contentElement.innerHTML = `
+                <input id="contentValue" type="text" placeholder="${content}">
+                <input id="submitChange" type="button" name="submit" value="confirm">
+                `
+                const submitBtn = document.getElementById('submitChange')
+                submitBtn.addEventListener('click', async (e) => {
+                    e.preventDefault()
+                    let changeInfo = {
+                        id: 0,
+                        data: {
+                            dataId: index,
+                            dataContent: {
+                                "content": document.getElementById('contentValue').value,
+                                "importance": "normal"
+                            }
+                        }
+                    }
+                    const response = await fetchChangeUserInfo(changeInfo)
+                    if(response.valid === true) {
+                        const data = await fetchGetUserInfo(info)
+                        contentElement.innerHTML = `<span id='content'> ${data.data[index].dataContent.content} </span>`
+                    }
+                })
+            })
+
+            methodsArea.appendChild(labelModify)
+            methodsArea.appendChild(modifyBtn)
+            methodsArea.appendChild(labelRemove)
+            methodsArea.appendChild(removeBtn)
+            dataBlock.appendChild(methodsArea)
             container.appendChild(dataBlock)
-        });
+        })
     } catch (error) {
         console.log(error)
     }
 }
 
-// getDataList(getInfo)
+getDataList(getInfo)
